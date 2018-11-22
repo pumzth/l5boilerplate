397e892b0daa580ebea1826bd1d08abe6d6d876b
Pariwat Panniyom <pariwat@netway.co.th>
2018-11-22 16:17:19 +0700

    first commit

---
+++ resources/js/frontend/components/ExampleComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <i class="fas fa-code"></i> Example Vue Component
+        </div>
+        <div class="card-body txt-color">
+            I'm an example Vue component!<br>
+            ทดสอบ HMR .vue 1
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+$color-1: rgb(25, 117, 40);
+.txt-color {
+    color: $color-1
+}
+</style>
---
+++ webpack.mix.js
@@ -0,0 +1,56 @@
+const mix = require('laravel-mix');
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.setPublicPath('public');
+
+mix.sass('resources/sass/frontend/app.scss', 'css/frontend.css')
+    .sass('resources/sass/backend/app.scss', 'css/backend.css')
+    .js('resources/js/frontend/app.js', 'js/frontend.js')
+    .js([
+        'resources/js/backend/before.js',
+        'resources/js/backend/app.js',
+        'resources/js/backend/after.js'
+    ], 'js/backend.js')
+    .extract([
+        'jquery',
+        'bootstrap',
+        'popper.js/dist/umd/popper',
+        'axios',
+        'sweetalert2',
+        'lodash',
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/free-brands-svg-icons',
+        '@fortawesome/free-regular-svg-icons',
+        '@fortawesome/free-solid-svg-icons'
+    ]);
+    // .extract([
+    //     '@fortawesome/fontawesome-svg-core',
+    //     '@fortawesome/free-brands-svg-icons',
+    //     '@fortawesome/free-regular-svg-icons',
+    //     '@fortawesome/free-solid-svg-icons'
+    // ],'js/commons.js');
+
+
+if (mix.inProduction() || process.env.npm_lifecycle_event !== 'hot') {
+    mix.version();
+}
+
+const webpackConfig = smp.wrap({
+
+});
+
+mix.webpackConfig(webpackConfig);
+
+// mix.disableNotifications();
